697ac7224f21f73410f3d675da54e6f24db170bd
af <agustinfiorenza@gmail.com>
2017-06-02 11:28:57 -0300

    Rename

--- index.android.js
+++ index.android.js
@@ -2,4 +2,4 @@ import React from 'react';
 import {AppRegistry} from 'react-native';
 import  App from './src/views'
 
-AppRegistry.registerComponent('HelloWorld', () => App);
+AppRegistry.registerComponent('EventosRosario', () => App);
